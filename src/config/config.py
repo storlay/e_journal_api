f821cd2befc6b23f371b60681bcddb911baa487a
Dmitrii Ivanov <dmitriy.site1@gmail.com>
2024-08-09 10:27:21 -0300

    Add project settings

---
+++ src/config/config.py
@@ -0,0 +1,42 @@
+import os
+
+from pydantic import (
+    BaseModel,
+    PostgresDsn,
+)
+from pydantic_settings import BaseSettings
+
+
+class DatabaseSettings(BaseModel):
+    NAME: str = os.getenv(
+        "POSTGRES_DB",
+        "example_db",
+    )
+    USER: str = os.getenv(
+        "POSTGRES_USER",
+        "admin",
+    )
+    PASS: str = os.getenv(
+        "POSTGRES_PASSWORD",
+        "password",
+    )
+    HOST: str = os.getenv(
+        "DB_HOST",
+        "db",
+    )
+    PORT: int = os.getenv(
+        "DB_PORT",
+        5432,
+    )
+    URL: PostgresDsn = f"postgresql+asyncpg://{USER}:{PASS}@{HOST}:{PORT}/{NAME}"
+
+    ECHO: bool = False
+    POOL_SIZE: int = 50
+    MAX_OVERFLOW: int = 10
+
+
+class Settings(BaseSettings):
+    db: DatabaseSettings = DatabaseSettings()
+
+
+settings = Settings()
