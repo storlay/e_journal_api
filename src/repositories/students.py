267d704e010a1df1c096899992f78ac5785a3867
Dmitrii Ivanov <dmitriy.site1@gmail.com>
2024-08-09 17:19:35 -0300

    Redefine getting a student by filters in the repository

--- src/repositories/students.py
+++ src/repositories/students.py
@@ -23,3 +23,16 @@ class StudentsRepository(BaseRepository):
         result = await self.session.execute(statement)
         result = [row[0].to_read_model() for row in result.all()]
         return result
+
+    async def find_one(self, **filter_by) -> StudentSchema:
+        """
+        Search for a student by filters in the database.
+        :param filter_by: Filters.
+        :return: Pydantic models representing the students.
+        """
+        statement = (
+            select(self.model)
+            .options(selectinload(self.model.scores))
+            .filter_by(**filter_by))
+        result = await self.session.execute(statement)
+        return result.scalar_one().to_read_model()
