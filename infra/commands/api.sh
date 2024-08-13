83689170aa08842d3fa2ddf5d62c9533397dd791
Dmitrii Ivanov <dmitriy.site1@gmail.com>
2024-08-13 15:56:31 -0300

    Add docker-compose for tests

--- infra/commands/api.sh
+++ infra/commands/api.sh
@@ -2,4 +2,8 @@
 
 alembic revision --autogenerate
 alembic upgrade head
-gunicorn src.main:app --worker-class uvicorn.workers.UvicornWorker --bind=0.0.0.0:8000
+if [[ "${1}" == "dev" ]]; then
+    gunicorn src.main:app --worker-class uvicorn.workers.UvicornWorker --bind=0.0.0.0:8000
+elif [[ "${1}" == "test" ]]; then
+    gunicorn src.main:app --worker-class uvicorn.workers.UvicornWorker --bind=0.0.0.0:8080
+fi
